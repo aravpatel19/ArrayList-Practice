8b49e72f6d2fc021237439073c72cc1ff76cfb7f
aravpatel19 <45393174+aravpatel19@users.noreply.github.com>
2018-11-27 08:47:29 -0500

    Update ArrayListRespitoryPractice.java

--- ArrayListRespitoryPractice.java
+++ ArrayListRespitoryPractice.java
@@ -3,7 +3,15 @@ import java.util.ArrayList;
 public class ArrayListRespitoryPractice{
 	public static void main(String[]args){
 
+		ArrayList<Integer> a = new ArrayList<>();
+		ArrayList<Integer> b = new ArrayList<>();
 
+		for(int i=0;i<10;i++)
+			a.add(i);
+		for(int i=10;i<20;i++)
+			b.add(i);
+
+		System.out.println(newList(a, b));
 	}
 	public static ArrayList<Integer> newList(ArrayList<Integer> a, ArrayList<Integer> b){
 
